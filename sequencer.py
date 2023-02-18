7b11eb5c8d65987186775d746fb3174fdaa784c8
D3stinn3 <paynedestinne@gmail.com>
2023-02-18 06:47:39 -0800

    Sequencing

---
+++ sequencer.py
@@ -0,0 +1,14 @@
+class Node:
+
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+
+class linkedList:
+
+    def __init__(self):
+        self.head = None
+
+
+"""The first class defines instances of elements in the linked chains, doesnt necessarily have to comprehend the next"""
