088edb18e41044853ae26acb7ab14c065e1a500f
D3stinn3 <paynedestinne@gmail.com>
2023-02-18 08:18:10 -0800

    Sequencing xt 1

--- sequencer.py
+++ sequencer.py
@@ -10,5 +10,12 @@ class linkedList:
     def __init__(self):
         self.head = None
 
+class chaiLink:
+    list = linkedList()
+    first_node = Node(1)
+    second_node = Node(2)
 
 """The first class defines instances of elements in the linked chains, doesnt necessarily have to comprehend the next"""
+
+# if "__name__" == "__main__":
+
